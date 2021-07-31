219d9ef31dc7871ed531b435859e85e02e39863e
James Driscoll <thejamesdriscoll@gmail.com>
2021-07-31 17:39:59 -0500

    set playRound parameters equal to choiceC and choiceP

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,37 +4,44 @@ function random(num) {
     return Math.floor(Math.random()*num)
 }
 function computerChoice() {
-    let choice = random(3)
-    if (choice == 0) {
+    let choiceC = random(3)
+    if (choiceC == 0) {
         return "rock"
-    } else if (choice == 1) {
+    } else if (choiceC == 1) {
         return "paper"
-    } else if (choice == 2) {
+    } else if (choiceC == 2) {
         return "scissors"
     }
     //allow player to choose and convert it to lowercase
 }
 function playerChoice () {
-    let choice = prompt("Choose Rock Paper or Scissors")
-    return choice.toLowerCase()
+    let choiceP = prompt("Choose Rock Paper or Scissors")
+    choiceP.toLowerCase()
+    if (choiceP == "rock" || choiceP == "paper" || choiceP == "scissors") {
+        return choiceP
+    } else return(console.log("Please choose either rock paper or scissors"))
+    
 }
 //compare player selection to computer selection and log the result
-console.log(computerChoice(),playerChoice())
-function playRound() {
-    if (computerChoice() == "rock" && playerChoice() == "paper") {
+function playRound(choiceC,choiceP) {
+    choiceC = playerChoice()
+    choiceP = computerChoice()
+    
+    if (choiceC == "rock" && choiceP == "paper") {
         console.log("You win. Paper beats Rock")
-    } else if (computerChoice() == "rock" && playerChoice() == "scissors") {
+    } else if (choiceC == "rock" && choiceP == "scissors") {
         console.log("You lose. Rock beats Scissors")
-    } else if (computerChoice() == "paper" && playerChoice() == "rock") {
+    } else if (choiceC == "paper" && choiceP == "rock") {
         console.log("You lose. Paper beats Rock")
-    } else if (computerChoice() == "paper" && playerChoice() == "scissors") {
+    } else if (choiceC == "paper" && choiceP == "scissors") {
         console.log("You win. Scissors beat Paper")
-    } else if (computerChoice() == "scissors" && playerChoice() == "paper") {
+    } else if (choiceC == "scissors" && choiceP == "paper") {
         console.log("You lose. Scissors beat paper")
-    } else if (computerChoice() == "scissors" && playerChoice() == "rock") {
+    } else if (choiceC == "scissors" && choiceP == "rock") {
         console.log("You win. Rock beats Scissors")
-    } else console.log("Something's not right")
+    } else if (choiceC == choiceP) {
+        console.log("The game was a tie.")
+    } else console.log("Something went wrong.")
 }
 playRound()
-
 
