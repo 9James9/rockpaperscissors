d3a3ce617f3ae077dfa8de23a9564d9c1f40d74f
James Driscoll <thejamesdriscoll@gmail.com>
2021-07-31 16:52:40 -0500

    created computer choice function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+// randomly select either "rock", "paper", or "scissors"
+//random number generator for 1-3, assigning each number value to a different computer choice
+function random(num) {
+    return Math.floor(Math.random()*num)
+}
+function computerChoice() {
+    let choice = random(3)
+    console.log(choice)
+    if (choice == 0) {
+        return "rock"
+    } else if (choice == 1) {
+        return "paper"
+    } else if (choice == 2) {
+        return "scissors"
+    }
+}
