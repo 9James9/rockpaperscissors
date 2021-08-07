6f8fd0af6dcf7a8345706d37ab5ac2f183df9402
James Driscoll <thejamesdriscoll@gmail.com>
2021-08-07 13:39:59 -0500

    added buttons that select choiceP = rock paper or scissors

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -19,19 +19,59 @@ function computerChoice() {
     }
     //allow player to choose and convert it to lowercase
 }
-
-function playerChoice() {
+/*
+  function playerChoice() {
     let choiceP = prompt("Choose Rock Paper or Scissors")
     choiceP.toLowerCase()
     if (choiceP == "rock" || choiceP == "paper" || choiceP == "scissors") {
         return choiceP
     } else return (console.log("Please choose either rock paper or scissors"))
-
 }
+*/
+
+//new player choice interface
+//created buttons with dom
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+content.classList.add('content')
+content.textContent = 'This is the container for three choices'
+container.appendChild(content)
+
+const button1 = document.createElement('button')
+button1.classList.add('rock')
+button1.textContent = 'Rock'
+content.appendChild(button1)
+
+const button2 = document.createElement('button')
+button2.classList.add('paper')
+button2.textContent = 'Paper'
+content.appendChild(button2)
+
+const button3 = document.createElement('button')
+button3.classList.add('scissors')
+button3.textContent = 'Scissors'
+content.appendChild(button3)
+//
+
+const playerChoseRock = function() {
+    playRound(computerChoice(),'rock')
+}
+const playerChosePaper = function() {
+    playRound(computerChoice(),'paper')
+}
+const playerChoseScissors = function() {
+    playRound(computerChoice(),'scissors')    
+}
+//adding event listener on button click
+button1.addEventListener('click',playerChoseRock)
+button2.addEventListener('click',playerChosePaper)
+button3.addEventListener('click',playerChoseScissors)
 //compare player selection to computer selection and log the result
 function playRound(choiceC, choiceP) {
-    choiceP = playerChoice()
-    choiceC = computerChoice()
+    choiceP
+  
+
+   // choiceC = computerChoice()
     console.log(`The computer chose: ${choiceC} and you chose: ${choiceP}`)
 
     if (choiceC == "rock" && choiceP == "paper") {
@@ -68,5 +108,7 @@ function game() {
         console.log("The game was a tie.")
     }
 }
-game()
+// game()
 console.log(`Total player wins: ${playerWins}. Total computer wins: ${computerWins}`)
+playRound()
+
